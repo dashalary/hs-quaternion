d605bad0bd71d217d692c4ecf424e4bb3aa4403a
Dasha Lary <dasha4988@gmail.com>
2021-05-22 14:04:54 -0400

    added Num instance

--- Quaternion.hs
+++ Quaternion.hs
@@ -6,5 +6,13 @@ data Quaternion = Q
   }
   deriving (Show)
 
+instance Num Quaternion where
+  q0 + q1 = Q (qR q0 + qR q1) (qI q0 + qI q1) (qJ q0 + qJ q1) (qK q0 + qK q1)
+  q0 * q1 = undefined
+  abs = undefined
+  signum = undefined
+  fromInteger = undefined
+  negate = undefined
+
 main :: IO ()
-main = print $ Q 1 2 3 4
+main = print $ Q 1 2 3 4 + Q 10 20 30 40
